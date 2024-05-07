26ff79773abb440ced1ac382852615e77d86cb5f
wenhuchen <hustchenwenhu@gmail.com>
2024-05-07 09:24:07 -0400

    update

---
+++ math_eval/run_7B_plus.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+model='TIGER-Lab/MAmmoTH2-7B-Plus'
+
+dataset='theoremqa'
+
+python run_open.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='math'
+
+python run_open.py \
+  --model $model \
+  --shots 4 \
+  --dataset $dataset \
+  --form short
+
+dataset='gsm8k'
+
+python run_open.py \
+  --model $model \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='gpqa_diamond'
+
+python run_choice.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --use_vllm \
+  --form mistral
+
+dataset='mmlu_stem'
+
+python run_choice.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --use_vllm \
+  --form short
+
+dataset='bbh'
+
+python run_open.py \
+  --model $model \
+  --shots 3 \
+  --dataset $dataset \
+  --use_vllm \
+  --form short:step
+
+dataset='arc'
+
+python run_choice.py \
+  --model $model \
+  --shots 8 \
+  --dataset $dataset \
+  --use_vllm \
+  --form short:step
