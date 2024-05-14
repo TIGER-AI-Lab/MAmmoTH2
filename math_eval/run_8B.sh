c28230920b45e79aa71ab70f2092db32a9b1afb1
wenhuchen <hustchenwenhu@gmail.com>
2024-05-13 22:15:35 -0400

    adding non plus version script for reproduction

---
+++ math_eval/run_8B.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+model='TIGER-Lab/MAmmoTH2-8B'
+
+dataset='theoremqa'
+
+python run_open.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='math'
+
+python run_open.py \
+  --model $model \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='gsm8k'
+
+python run_open.py \
+  --model $model \
+  --shots 4 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='gpqa_diamond'
+
+python run_choice.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='mmlu_stem'
+
+python run_choice.py \
+  --model $model \
+  --shots 5 \
+  --dataset $dataset \
+  --form short
+
+dataset='bbh'
+
+python run_open.py \
+  --model $model \
+  --shots 3 \
+  --dataset $dataset \
+  --form short:step
+
+dataset='arc'
+
+python run_choice.py \
+  --model $model \
+  --shots 8 \
+  --dataset $dataset \
+  --form short
