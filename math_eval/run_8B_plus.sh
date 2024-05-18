d5109bc272399e005a482da97a596077c716d037
wenhu chen <wenhuchen@uwaterloo.ca>
2024-05-18 10:24:55 -0400

    Update run_8B_plus.sh

--- math_eval/run_8B_plus.sh
+++ math_eval/run_8B_plus.sh
@@ -16,7 +16,7 @@ python run_open.py \
   --model $model \
   --shots 4 \
   --dataset $dataset \
-  --form short:step
+  --form short
 
 dataset='gsm8k'
 
@@ -40,7 +40,7 @@ python run_choice.py \
   --model $model \
   --shots 5 \
   --dataset $dataset \
-  --form short:step
+  --form short
 
 dataset='bbh'
 
@@ -48,7 +48,7 @@ python run_open.py \
   --model $model \
   --shots 3 \
   --dataset $dataset \
-  --form short
+  --form short:step
 
 dataset='arc'
 
